a3cd859f2787ef604487b99e4ad8304a3e1ffb99
Jon Koenig <jkoenig@outlook.com>
2025-11-12 23:24:12 -0700

    add sourcekit

--- nvim/lua/_lsp.lua
+++ nvim/lua/_lsp.lua
@@ -1,3 +1,3 @@
 --vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.format()' -- Format on save.
-vim.lsp.enable({ "lua_ls", "zls", "clangd", "rust_analyzer", "gopls" })
+vim.lsp.enable({ "lua_ls", "zls", "clangd", "rust_analyzer", "gopls", "sourcekit" })
 vim.diagnostic.config({ virtual_text = true })
