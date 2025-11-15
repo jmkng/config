b095c863c19c0d50b8031398bb1affce475e6142
Jon Koenig <jkoenig@outlook.com>
2025-11-14 19:17:58 -0700

    fix name <:)

--- nvim/colors/scarecrow.lua
+++ nvim/colors/scarecrow.lua
@@ -3,7 +3,7 @@ if vim.fn.exists("syntax_on") == 1 then
     vim.cmd("syntax reset")
 end
 
-vim.g.colors_name = "rainyday"
+vim.g.colors_name = "scarecrow"
 
 local palette = {
     _DEBUG             = "#ff0000",
