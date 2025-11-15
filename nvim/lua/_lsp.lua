3fce62a2b1fe05cd4206154d7bf25629034e096d
Jon Koenig <jkoenig@outlook.com>
2025-11-14 19:20:44 -0700

    disable virtual text by default

--- nvim/lua/_lsp.lua
+++ nvim/lua/_lsp.lua
@@ -1,3 +1,3 @@
 --vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.format()' -- Format on save.
 vim.lsp.enable({ "lua_ls", "zls", "clangd", "rust_analyzer", "gopls", "sourcekit" })
-vim.diagnostic.config({ virtual_text = true })
+vim.diagnostic.config({ virtual_text = false })
