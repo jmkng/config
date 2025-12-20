111387e4512e7638df124e8bcec9db6f541b5be1
Jon Koenig <jkoenig@outlook.com>
2025-12-20 09:53:26 -0700

    remove user commands in favor of builtins

--- nvim/lua/command.lua
+++ nvim/lua/command.lua
@@ -7,44 +7,13 @@ end, { desc = "Edit editor user keymap" })
 vim.api.nvim_create_user_command('Command', function()
     vim.cmd("edit " .. vim.fn.stdpath("config") .. "/" .. "lua" .. "/" .. "command.lua")
 end, { desc = "Edit editor user commands" })
-vim.api.nvim_create_user_command("Reload", function()
-    vim.cmd("luafile $MYVIMRC")
-    if vim.g.colors_name then
-        vim.cmd("color " .. vim.g.colors_name)
-    end
-    -- Would be nice to have a way to reload treesitter queries,
-    -- not sure how to make that work.
-end, { desc = "Reload editor configuration" })
 vim.api.nvim_create_user_command('Trim', function()
     vim.cmd([[%s/\s\+$//e]])
 end, { desc = "Trim whitespace in current buffer" })
 
-vim.api.nvim_create_user_command('CursorLine', function()
-    vim.o.cursorline = not vim.o.cursorline
-end, { desc = " cursorline" })
-vim.api.nvim_create_user_command('CursorColumn', function()
-    vim.o.cursorcolumn = not vim.o.cursorcolumn
-end, { desc = " vertical cursorline" })
-vim.api.nvim_create_user_command('LineNumber', function()
-    vim.wo.number = not vim.wo.number
-end, { desc = " line numbers" })
-vim.api.nvim_create_user_command('SignColumn', function()
-    if vim.wo.signcolumn == "yes" then
-        vim.wo.signcolumn = "no"
-    else
-        vim.wo.signcolumn = "yes"
-    end
-end, { desc = "Toggle sign column" })
-vim.api.nvim_create_user_command('ColorColumn', function(opts)
-    if opts.args == "" then
-        vim.o.colorcolumn = ""
-    else
-        vim.o.colorcolumn = opts.args
-    end
-end, {
-    desc = "Set or  color column",
-    nargs = "?"
-})
-vim.api.nvim_create_user_command('', function()
+vim.api.nvim_create_user_command('TrimWhitespace', function()
     vim.cmd([[%s/\s\+$//e]])
 end, { desc = "Trim whitespace in current buffer" })
+vim.api.nvim_create_user_command('BufOnly', function()
+  vim.cmd("%bd|e#|bd#")
+end, { desc = "Close all buffers except the current" })
